4e2913e064dcbf028539ce2a6a83899a29deda75
bogyuni <bogyuni@hotmail.com>
2023-03-20 22:50:57 +0900

    link

--- js/common.js
+++ js/common.js
@@ -1,8 +1,8 @@
 window.addEventListener('DOMContentLoaded', function(){
-	fetch('/inc/header.html')
+	fetch('./inc/header.html')
 		.then(response => { return response.text(); })
 		.then(data => { document.getElementById('header').innerHTML = data; });
-	fetch('/inc/footer.html')
+	fetch('./inc/footer.html')
 		.then(response => { return response.text(); })
 		.then(data => { document.getElementById('footer').innerHTML = data; });
 
