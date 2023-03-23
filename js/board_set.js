dc98b3aec90ad75e9dd9daa749ecb70eb9800e39
bogyuni <bogyuni@hotmail.com>
2023-03-23 21:54:53 +0900

    page program

--- js/board_set.js
+++ js/board_set.js
@@ -1,42 +1,65 @@
 window.addEventListener('load', function(){
+	// 게시물 DOM 생성
+	boardConDomSet();
+
+	// 스케쥴 게시물 클릭
+	boardConClickSet();
+});
+
+function boardConClickSet() {
+	const boardCon = this.document.querySelectorAll('.board-con');
+	for (let i = 0; i < boardCon.length; i++) {
+		boardCon[i].querySelector('.top-con').addEventListener('click', function(){
+			if (!this.parentNode.classList.contains('on')) {
+				for (let j = 0; j < boardCon.length; j++) {
+					boardCon[j].classList.remove('on');
+				}
+				this.parentNode.classList.add('on');
+			} else {
+				this.parentNode.classList.remove('on');
+			}
+		});
+	}
+}
+
+
+function boardConDomSet() {
 	const boardList = document.getElementById('boardList');
 	let boardConDom = [];
 	let inTitle = '';
 	let boardType = '';
-
-	// 월 표시
-	const currentMonth = parseInt(window.location.search.split('month=')[1]);
-	const monthNum = document.getElementsByClassName('month-num')[0];
-	monthNum.innerHTML = currentMonth;
-
-	// 다음 월, 이전 월 링크 세팅
-	const linkNext = document.getElementsByClassName('link-next')[0];
-	const linkPrev = document.getElementsByClassName('link-prev')[0];
-	const path = window.location.origin + window.location.pathname;
-	const nextMonth = currentMonth == 12 ? 1 : currentMonth + 1;
-	const prevMonth = currentMonth == 1 ? 12 : currentMonth - 1;
-	linkNext.href = path+'?month=' + nextMonth;
-	linkPrev.href = path+'?month=' + prevMonth;
-
-	if (currentPageName === 'performance') {
+	let pathName = window.location.pathname;
+	let menuName = '';
+	// 서브페이지 확인
+	if (pathName.indexOf('performance') > -1) {
+		menuName = 'performance';
+	} else if (pathName.indexOf('education') > -1) {
+		menuName = 'education';
+	} else if (pathName.indexOf('tickets') > -1) {
+		menuName = 'tickets';
+	} else if (pathName.indexOf('guide') > -1) {
+		menuName = 'guide';
+	}
+	
+	if (menuName === 'performance') {
 		inTitle = ['일시','장소', '관람료'];
-	} else if (currentPageName === 'education') {
+	} else if (menuName === 'education') {
 		inTitle = ['대상','강사', '수강료'];
 	}
 
 	for (let i = 0; i < schedule.length; i++) {
-		if (schedule[i].category == '대표공연'){
-			boardType = 'brand';
-		} else if (schedule[i].category == '정기공연'){
-			boardType = 'regular';
-		} else if (schedule[i].category == '기획공연'){
-			boardType = 'special';
-		} else if (schedule[i].category == '상설공연'){
-			boardType = 'permanent';
+		if (schedule[i].category == '대표공연' || schedule[i].category == '교육전문가'){
+			boardType = 'type1';
+		} else if (schedule[i].category == '정기공연' || schedule[i].category == '일반인'){
+			boardType = 'type2';
+		} else if (schedule[i].category == '기획공연' || schedule[i].category == '청소년'){
+			boardType = 'type3';
+		} else if (schedule[i].category == '상설공연' || schedule[i].category == '외국인'){
+			boardType = 'type4';
 		} else if (schedule[i].category == '명절공연'){
-			boardType = 'seasonal';
+			boardType = 'type5';
 		} else if (schedule[i].category == '교류공연'){
-			boardType = 'exchange';
+			boardType = 'type6';
 		}
 
 		boardConDom[i] =
@@ -44,7 +67,7 @@ window.addEventListener('load', function(){
 			<div class="top-con">
 				<div class="day-con">${schedule[i].day}</div>
 				<div class="week-con">${schedule[i].week}</div>
-				<div class="category-con ${boardType}">${schedule[i].category}</div>
+				<div class="category-con">${schedule[i].category}</div>
 				<div class="title-con">${schedule[i].title}</div>
 			</div>
 			<div class="bot-con">
@@ -62,27 +85,10 @@ window.addEventListener('load', function(){
 						<dt>${inTitle[2]}</dt>
 						<dd>${schedule[i].inTit3}</dd>
 					</dl>
-					<a class="link-more" href="${schedule[i].link}">더보기</a>
+					<a class="link-more" href="${schedule[i].link}?month=${currentMonth}&dayPage">더보기</a>
 				</div>
 			</div>
 		</li>`;
 		boardList.innerHTML += boardConDom[i];
 	}
-
-
-	// 스케쥴 게시물 클릭
-	const boardCon = this.document.querySelectorAll('.board-con');
-	for (let i = 0; i < boardCon.length; i++) {
-		boardCon[i].querySelector('.top-con').addEventListener('click', function(){
-			if (!this.parentNode.classList.contains('on')) {
-				for (let j = 0; j < boardCon.length; j++) {
-					boardCon[j].classList.remove('on');
-				}
-				this.parentNode.classList.add('on');
-			} else {
-				this.parentNode.classList.remove('on');
-			}
-		});
-	}
-	
-});
+}
--- js/sub.js
+++ js/sub.js
@@ -1,4 +1,22 @@
 // 스케쥴 카테고리 정렬
+
+window.addEventListener('load', function(){
+	// 월 표시
+	const currentMonth = parseInt(window.location.search.split('month=')[1]);
+	const monthNum = document.getElementsByClassName('month-num')[0];
+	monthNum.innerHTML = currentMonth;
+
+	// 다음 월, 이전 월 링크 세팅
+	const linkNext = document.getElementsByClassName('link-next')[0];
+	const linkPrev = document.getElementsByClassName('link-prev')[0];
+	const path = window.location.origin + window.location.pathname;
+	const nextMonth = currentMonth == 12 ? 1 : currentMonth + 1;
+	const prevMonth = currentMonth == 1 ? 12 : currentMonth - 1;
+	linkNext.href = path+'?month=' + nextMonth;
+	linkPrev.href = path+'?month=' + prevMonth;
+});
+
+
 function sortCategory(category, btn) {
 	const boardList = this.document.querySelectorAll('.board-con');
 	const btnCategory = this.document.querySelectorAll('.btn-category');
@@ -20,3 +38,18 @@ function sortCategory(category, btn) {
 	}
 	btn.classList.add('on');
 }
+
+// const currentPageName = 'performance';
+const currentMonth = window.location.search.split('month=')[1];
+const path = window.location.origin + window.location.pathname;
+
+console.log(window.location.search.indexOf('dayPage'));
+
+const currentPageDepth = window.location.search.indexOf('dayPage') > -1 ? 'dayPage' : 'subPage';
+if (currentPageDepth == 'subPage'){
+	const head = document.getElementsByTagName('head')[0];
+	const script = document.createElement('script');
+	script.src = path+'month_'+currentMonth+'.js';
+	head.appendChild(script);
+}
+
