b138727f486c7cead8fd3be14bc0940f679e4b35
bogyuni <bogyuni@hotmail.com>
2023-03-28 03:45:50 +0900

    tickets add

--- bkm/en/education/month_1.js
+++ bkm/en/education/month_1.js
@@ -5,9 +5,9 @@ let schedule = [
 		category: 'Educational expert',
 		title: '교사 국악직무연수 Gugak Training for Teachers  ',
 		imgSrc: '0110.png',
-		inTit1: '전국 초중등 교원',
-		inTit2: '국립부산국악원 연주단원, 외부 강사',
-		inTit3: '30,000원',
+		inTit1: 'Elementary, Middle, and High school Teachers',
+		inTit2: 'Busan National Gugak Center Musicians, Invited instructors',
+		inTit3: '30,000won',
 		link: '0110.html'
 	},
 	
